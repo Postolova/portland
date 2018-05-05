244bce40ed6c9a6a7cc6c45e245d933e8bdaae55
MaryKa <marykaweb@gmail.com>
2018-05-05 17:24:59 +0300

    add left information menu, aviable, brand, pagination, opinion, footer

--- build/js/main.js
+++ build/js/main.js
@@ -1,1 +1,6 @@
-console.log('Проверка')
+$( document ).ready( function() {
+	$( ' .information-menu__items' ).on( 'click', function() {
+		$('.information-menu-second').removeClass('visible'); 
+		$(this).next(' .information-menu-second ').toggleClass(' visible ');
+	});
+});
